db702fe1918385197e444f1c5d42db1ed1f154d8
fr3d100 <f.bonnand@gmail.com>
2020-11-10 16:01:19 +0100

    Create exercices

---
+++ lib/1_basics.rb
@@ -0,0 +1,32 @@
+
+
+# 1 - Déclarartion d'une chaine de caractère
+# Déclarer une variable = votre prénom
+
+# 2 - Déclaration d'un nombre entier
+#Déclarer une variable = votre année de naissance
+
+# 3 - Opération basique 
+# Calculer en une ligne de votre année de naissance en fonction de votre age en 2020
+
+# 4 - Ecrire une méthode
+# Ecrire une fonction qui, à partir de votre année de réponse et de votre prénom, retourne : 
+      # - "Bonjour, [votre prénome], en 2020 vous avez eu [age en 2020] ans"
+
+
+# 5 - Ecrire un if/else 
+# Ecrire une méthode qui à partir de votre année de naissance répond : 
+#       - "Vous avez le droit de boire une bière" si votre age en 2020 est d'au moins 18 ans
+#       - "Vous n'avez pas le droit de boire une bière" si vous avez moins de 18 ans en 2020
+
+# 6 - Déclarer un tableau de valeurs
+# Déclarer dans un tableau le nom des 3 neuveux de Picsou
+
+
+# 7 - Boucle
+# Afficher au travers d'une boucle le prénom de chaucun des neuveux de Picsou
+
+
+# 8 - Case switch
+# Parcours tous les prénom des neuveux de Picsou. Si le prénom à moins de 5 lettres, afficher 
+# "[PRENOM DU NEUVEUX] est facile à retenir", sinon afficher "[PRENOM DU NEVEUX], c'est déjà plus compliqué !"
